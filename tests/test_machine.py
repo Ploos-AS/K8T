3416ba9d3a8134fbb78176b85f0b96a80122ad26
pos <pgousdal@users.noreply.github.com>
2026-09-18 04:46:31 +0200

    Update machine tests for W65C265S M2 platform

--- tests/test_machine.py
+++ tests/test_machine.py
@@ -1,36 +1,43 @@
 import unittest
 from src.k8t_machine import *
 
-class M1Tests(unittest.TestCase):
-    def test_reset_state(self):
+class M2PlatformTests(unittest.TestCase):
+    def test_24_bit_address_wrap(self):
         m=K8TMachine()
-        self.assertEqual(m.pc,0xF000)
-        self.assertEqual(m.sp,0xDFFF)
-        self.assertTrue(m.supervisor)
+        self.assertEqual(m.normalize_address(0x1234567),0x234567)
+
+    def test_native_memory_regions(self):
+        m=K8TMachine()
+        self.assertEqual(m.region(0x000100),"bank0")
+        self.assertEqual(m.region(0x010000),"ram")
+        self.assertEqual(m.region(0x800000),"expansion")
+        self.assertEqual(m.region(0xE00000),"io")
+        self.assertEqual(m.region(0xF00000),"rom")
+
+    def test_reset_then_native_mode(self):
+        m=K8TMachine()
+        self.assertTrue(m.emulation)
+        self.assertFalse(m.native_mode)
+        m.enter_native_mode()
+        self.assertTrue(m.native_mode)
+        self.assertFalse(m.emulation)
+
+    def test_primary_ram_is_directly_addressed(self):
+        m=K8TMachine()
+        m.write8(0x010000,0x11)
+        m.write8(0x020000,0x22)
+        self.assertEqual(m.read8(0x010000),0x11)
+        self.assertEqual(m.read8(0x020000),0x22)
 
     def test_rom_is_read_only(self):
-        m=K8TMachine(); m.rom[0]=0x42
-        m.write8(0xF000,0x99)
-        self.assertEqual(m.read8(0xF000),0x42)
-
-    def test_banked_window(self):
         m=K8TMachine()
-        m.write8(BANK_REG_LO,1); m.write8(0x8000,0x11)
-        m.write8(BANK_REG_LO,2); m.write8(0x8000,0x22)
-        m.write8(BANK_REG_LO,1); self.assertEqual(m.read8(0x8000),0x11)
-        m.write8(BANK_REG_LO,2); self.assertEqual(m.read8(0x8000),0x22)
+        m.rom[0]=0x42
+        m.write8(ROM_BASE,0x99)
+        self.assertEqual(m.read8(ROM_BASE),0x42)
 
-    def test_irq_write_one_to_clear(self):
-        m=K8TMachine(); m.irq_pending=0b1111
-        m.write8(IRQ_PENDING,0b0101)
-        self.assertEqual(m.irq_pending,0b1010)
-
-    def test_four_uart_windows(self):
+    def test_four_integrated_uart_channels_are_baseline(self):
         m=K8TMachine()
-        for n in range(4):
-            a=UART_BASE+n*UART_STRIDE
-            m.write8(a,n+1)
-            self.assertEqual(m.read8(a),n+1)
+        self.assertEqual(len(m.uart),4)
 
 if __name__=="__main__":
     unittest.main()
